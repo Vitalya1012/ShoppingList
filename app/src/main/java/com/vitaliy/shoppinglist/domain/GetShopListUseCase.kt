774980ff8542be07aafd5cc44190984262ad8303
Vitaliy Shutko <vitaliashutko@gmail.com>
2022-08-18 21:52:52 +0300

    add domain

---
+++ app/src/main/java/com/vitaliy/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,7 @@
+package com.vitaliy.shoppinglist.domain
+
+class GetShopListUseCase (private val shopListRepository: ShopListRepository) {
+    fun getShopList():List<ShopItem>{
+        return shopListRepository.getShopList()
+    }
+}
